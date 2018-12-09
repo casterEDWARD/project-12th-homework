f6b941e5052e198c6411023aad9ab54a041cbafd
casterEDWARD <45506075+casterEDWARD@users.noreply.github.com>
2018-12-10 00:49:10 +0800

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,15 @@
+
+
+            String line=null;
+            while((line=br.readLine())!=null){
+                System.out.println(line);
+            }
+
+            br.close();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
